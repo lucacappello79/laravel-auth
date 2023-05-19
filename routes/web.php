2b2b9de02e947e5018208c93c9ca9f29ca97b6df
lucacappello79 <postt79@gmail.com>
2023-05-19 17:15:18 +0200

    feat: updating routes and controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+Route::get('/admin', [DashboardController::class, 'home'])->middleware(['auth', 'verified']);
 
 /*
 Route::get('/dashboard', function () {
@@ -26,13 +27,11 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 */
 
-
-/*
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-*/
+
 
 require __DIR__ . '/auth.php';
