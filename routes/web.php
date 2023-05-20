c7c20037a73df31ea746ab7f2595afcbc46d7f61
lucacappello79 <postt79@gmail.com>
2023-05-20 11:27:35 +0200

    creating projectcontroller route, updating seeder/table

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -32,6 +33,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::resource('projects', ProjectController::class)
 
 
 require __DIR__ . '/auth.php';
