b11a7e13757474bc207b4c5934bc37a99e103c98
lucacappello79 <postt79@gmail.com>
2023-05-20 12:36:18 +0200

    feat: added show.blade updated projectcontroller show

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,31 +2,27 @@
 
 @section('content')
 <table class="mt-5 table table-striped">
-    
+
     <thead>
-      <th>
-        Titolo
-      </th>
-      <th>Contenuto</th>
-      <th>
-        Slug
-      </th>
-      <th>
-        Comandi
-      </th>
+      <th>Title</th>
+      <th>Language</th>
+      <th>Content</th>
+      <th>Slug</th>
+      <th>Console</th>
+
     </thead>
   
     <tbody>
   
-      @foreach($posts as $post)
+      @foreach($projects as $item)
       <tr >
-        <td>{{$post->title}}</td>
-        <td>{{$post->content}}</td>
-        <td>{{$post->slug}}</td>
-        <td><a href="{{route('admin.posts.show', $post->slug)}}"><i class="fa-solid fa-magnifying-glass"></i></a></td>
+        <td>{{$item->title}}</td>
+        <td>{{$item->type}}</td>
+        <td>{{$item->content}}</td>
+        <td>{{$item->slug}}</td>
+        <td><a href="{{route('admin.projects.show', $project->slug)}}"><i class="fa-solid fa-magnifying-glass"></i></a></td>
       </tr>
       @endforeach
-  
   
     </tbody>
 </table>
