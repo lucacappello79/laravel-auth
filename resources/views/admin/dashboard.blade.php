235885c00500f8f165246cb569e6d2580322682e
lucacappello79 <postt79@gmail.com>
2023-05-21 16:29:44 +0200

    adding fillable to model

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts/admin')
+{{-- @extends('layouts/admin')
 
 @section('content')
   <h1>Admin page</h1>
@@ -8,4 +8,23 @@
   <ul>
     <li><a href="{{route('admin.projects.index')}}">Show all projects</a></li>
   </ul>
+@endsection --}}
+
+@extends('layouts/admin')
+
+@section('content')
+  <h1>Admin page</h1>
+
+  <hr>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <a href="{{route('admin.projects.index')}}" class="btn btn-primary">Manage Projects</a>
+      </div>
+      {{-- <div class="col-md-6">
+        <a href="{{route('admin.users.index')}}" class="btn btn-primary">Manage Users</a>
+      </div> --}}
+    </div>
+  </div>
 @endsection
